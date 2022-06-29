696e282a1dc9425e3698a2929a95b96064344ede
Carolinne Costa <carolinne.costa@unimedfortaleza.com.br>
2022-06-29 12:22:58 -0300

    feature - calculadora criada no caminho feliz :D

--- index.js
+++ index.js
@@ -14,40 +14,61 @@ keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
       const key = e.target
       Array.from(key.parentNode.children)
-      .forEach(k => k.classList.remove('is-depressed'))
-  }
-
-  if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    key.classList.add('is-depressed')
-
+        .forEach(k => k.classList.remove('is-depressed'))
     }
-    if (action === 'decimal') {
-      console.log('decimal key!')
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-depressed')
+      calculadora.dataset.previousKeyType = 'operator'
+      calculadora.dataset.firstValue = displayedNum
+      calculadora.dataset.operator = action
     }
+
+    const previousKeyType = calculadora.dataset.previousKeyType
 
     if (action === 'clear') {
       console.log('clear key!')
     }
 
-    if (action === 'calculate') {
-      console.log('equal key!')
+    if (!action) {
+      if (displayedNum === '0' || previousKeyType === 'operator') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent
+      }
     }
-  }
+
+    const calculate = (n1, operator, n2) => {
+      let result = ''
+  
+      if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2)
+      } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2)
+      } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2)
+      } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2)
+      }
+      
+      return result
+    }
+
+    if (action === 'calculate') {
+      const firstValue = calculadora.dataset.firstValue
+      const operator = calculadora.dataset.operator
+      const secondValue = displayedNum
+      
+      display.textContent = calculate(firstValue, operator, secondValue)
+    }
 
   if (action === 'decimal') {
     display.textContent = displayedNum + '.'
   }
-
-  if (!action) {
-    if (displayedNum === '0') {
-      display.textContent = keyContent
-    } else {
-      display.textContent = displayedNum + keyContent
-    }
   }
 })
