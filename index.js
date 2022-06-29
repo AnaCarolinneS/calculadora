0cda979ef98c0b037debc6d6d843f4c87b341ac2
Carolinne Costa <carolinne.costa@unimedfortaleza.com.br>
2022-06-29 12:09:28 -0300

    feature - numeros inseridos aparecem no display + valor decimal

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-const calculadora = document.querySelector('.calculator')
-const keys = calculadora.querySelector('.calculator__keys')
+const calculadora = document.querySelector('.calculadora')
+const keys = calculadora.querySelector('.calculadora__keys')
+const display = document.querySelector('.calculadora__display')
 
 keys.addEventListener('click', e => {
 
-const key = e.target
-const action = key.dataset.action
+  const key = e.target
+  const action = key.dataset.action
+  const keyContent = key.textContent
+  const displayedNum = display.textContent
 
+
+  if (e.target.matches('button')) {
     if (e.target.matches('button')) {
-        if (!action) {
-            console.log('number key!')
-          }
-        if (
-            action === 'add' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'
-          ) {
-            console.log('operator key!')
-          }
-        if (action === 'decimal') {
-            console.log('decimal key!')
-          }
-          
-          if (action === 'clear') {
-            console.log('clear key!')
-          }
-          
-          if (action === 'calculate') {
-            console.log('equal key!')
-          }
+      const key = e.target
+      Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+  }
+
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    key.classList.add('is-depressed')
+
     }
+    if (action === 'decimal') {
+      console.log('decimal key!')
+    }
+
+    if (action === 'clear') {
+      console.log('clear key!')
+    }
+
+    if (action === 'calculate') {
+      console.log('equal key!')
+    }
+  }
+
+  if (action === 'decimal') {
+    display.textContent = displayedNum + '.'
+  }
+
+  if (!action) {
+    if (displayedNum === '0') {
+      display.textContent = keyContent
+    } else {
+      display.textContent = displayedNum + keyContent
+    }
+  }
 })
